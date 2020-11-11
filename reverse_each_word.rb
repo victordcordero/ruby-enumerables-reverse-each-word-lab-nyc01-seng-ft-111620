5cb49888fb3172bd2a6a5632eb1a78ddddd2bfd4
Victor Cordero <victordcordero@gmail.com>
2020-11-11 20:18:20 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,8 @@
 def reverse_each_word_with_each(string)
-  origional_array = string.split(" ")
-  return_array = []
-  origional_array.each do |string|
-    return_array << string.reverse
-    return_array.join(" ")
-  end
-  return_array.join(" ")
-end
+  original_array = string.split(" ")
+   return_array = []
+   original_array.each do|string|
+     return_array << string.reverse
+   end
+   return_array.join(" ")
+ end
