94e6568eb7a85ae2ee89abfc975331726b60366d
Victor Cordero <victordcordero@gmail.com>
2020-11-11 20:16:53 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,8 @@
+def reverse_each_word_with_each(string)
+  origional_array = string.split("")
+  return_array = []
+  origional_array.each do |string|
+    return_array.join("")
+  end
+  return_array.join("")
+end
